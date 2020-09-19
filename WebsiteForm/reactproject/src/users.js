b5d8310ac81ca7fc5011985eba36142846d71fb2
Farjana <farjana09khan@gmail.com>
2020-09-19 06:41:59 -0700

    Initial commit

---
+++ WebsiteForm/reactproject/src/users.js
@@ -0,0 +1,14 @@
+const data = [
+    {
+        id: 1,
+        name: 'Farjana',
+        username: 'farjana12'
+    },
+    {
+        id: 2,
+        name: 'Inaya',
+        username: 'Inaya123'
+    }
+]
+
+export default data;
