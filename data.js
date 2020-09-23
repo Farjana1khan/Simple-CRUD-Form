e6e6d6e64dd7e7079475e1bd02a0e1cce2bb44a2
Farjana1khan <69414296+Farjana1khan@users.noreply.github.com>
2020-09-23 01:10:12 -0700

    Initial commit

---
+++ data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 0,
+        name: 'Farjana khan',
+        username: 'farjana@gmail.com'
+    },
+    {
+        id: 1,
+        name: 'Hunain khan',
+        username: 'hunain@gmail.com'
+    }
+];
+
+export default userList;
